0bd17d5487e8b302cc5727eae865b8637e33ffe2
Robert <roberto.luna2021@gmail.com>
2024-03-23 04:05:02 -0500

    Setup preact including theme changer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://dev.astro-blog-template-01.pages.dev",
+  integrations: [tailwind(), preact()],
+  site: "https://dev.astro-blog-template-01.pages.dev"
 });
